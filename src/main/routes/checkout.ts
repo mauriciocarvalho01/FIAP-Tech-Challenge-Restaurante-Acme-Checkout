1811f80144e9a812c36beb9c10d1ec3629808776
Mauricio Carvalho <developer.mauricio1@gmail.com>
2024-12-02 19:20:45 -0300

    build: change service

--- src/main/routes/checkout.ts
+++ src/main/routes/checkout.ts
@@ -5,12 +5,11 @@ import {
 } from '@/main/adapters';
 import { makePaymentController } from '@/main/factories/application/controllers';
 
-import { auth } from '@/main/middlewares';
+// import { auth } from '@/main/middlewares';
 
 import { Router } from 'express';
 
 export default (router: Router): void => {
-  router.get('/checkout', auth, getCheckout(makePaymentController()));
-  router.post('/checkout', auth, createCheckout(makePaymentController()));
-  router.post('/checkout/webhook', updatePaymentStatus(makePaymentController()));
+  router.get('/checkout',/*auth,*/ getCheckout(makePaymentController()));
+  router.post('/checkout', /*auth,*/ createCheckout(makePaymentController()));
 };
